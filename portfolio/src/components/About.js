4a4fbf7895adc6b843d06f8f3e92cdade38f7243
Georgeoak03 <101403429+Georgeoak03@users.noreply.github.com>
2022-11-17 18:42:08 +0000

    positioned text

--- portfolio/src/components/About.js
+++ portfolio/src/components/About.js
@@ -4,14 +4,13 @@ export default function About() {
     return (
         <section id="about-me">
             <div className="bg-zinc-900 h-screen flex">
-
-                <div className="flex-wrap-col m-auto md:m-0">
-                    <div className="flex-wrap-col w-2/5 relative z-10 left-1/4 md:top-32 md:left-40">
+                <div className="flex-wrap-col m-0">
+                    <div className="flex-wrap-col w-2/5 relative z-10 top-[15%] left-[20%] md:top-32 md:left-40">
                         <h1 className="text-6xl lg:text-12xl md:text-8xl text-white font-['Great_Sailor']">
                             About me.
                         </h1>
                     </div>
-                    <div className="flex-wrap-col relative z-10 w-4/5 md:w-1/3 left-9 md:left-1/4 top-10 md:top-52">
+                    <div className="flex-wrap-col relative z-10 w-4/5 md:w-1/3 left-9 md:left-1/4 top-[30%] md:top-52">
                         <p className="text-sm text-white font-['Futura'] md:text-xl">I am a motivated and passionate
                             Full Stack Developer. Outside of catchin' 'mons, chuckin' blue spikey shells and levatating
                             spaceships out of swamps, I have a love for the outdoors and getting to see the world.</p>
