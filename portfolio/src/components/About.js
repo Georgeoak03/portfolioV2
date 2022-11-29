eb6c44b961ec6a81e30701a5ddc2fb72cf06fe02
Georgeoak03 <101403429+Georgeoak03@users.noreply.github.com>
2022-11-29 10:11:53 +0000

    About md

--- portfolio/src/components/About.js
+++ portfolio/src/components/About.js
@@ -12,26 +12,42 @@ export default function About() {
                 <div className="invisible absolute z-0 bg-[#222A23] w-[70%] h-[475px] top-44 left-[15%] md:visible md:top-32 md:h-[700px] md:left-[20%]"></div>
                 <div className="absolute z-0 bg-[#263d28] w-[35%] h-6 top-40 left-[25%] md:h-10 md:left-[16%] md:top-44 md:w-[450px]"></div>
                 <div className="absolute w-[20%] h-6 z-0 bg-[#263d28] top-56 left-[25%] md:hidden"></div>
-                <div className="w-36 flex absolute z-0 left-[55%] top-52">
-                    <img src={photoOfMe} alt="George" className="grayscale opacity-25"/>
+                <div className="w-36 flex absolute z-0 left-[55%] top-52 md:w-72 md:left-[60%] md:top-72">
+                    <img src={photoOfMe} alt="George" className="grayscale opacity-25 md:opacity-75"/>
                 </div>
                 <div className="relative z-10 w-4/5 top-32 left-[20%] md:top-32 md:left-[12%]">
                     <h1 className="text-6xl w-2/5 md:w-full lg:text-12xl md:text-8xl text-white font-['Great_Sailor']">
                         About me.
                     </h1>
                 </div>
-                <div className="relative flex-wrap-col left-[10%] top-56 z-10 w-4/5 md:w-2/3 md:top-40 md:left-[22%]">
+                <div className="relative flex-wrap-col left-[10%] top-56 z-10 w-4/5 md:w-1/3 md:top-40 md:left-[22%]">
                     <p className="text-sm text-white font-['Futura'] md:text-lg lg:text-lg">I am a motivated and passionate
                         Full Stack Developer. Outside of catchin' 'mons, chuckin' blue spikey shells and levatating
                         spaceships out of swamps, I have a love for the outdoors and getting to see the world.</p>
                     <br />
-                    <p className="text-sm text-white font-['Futura'] md:text-xl">Technology has always interested
+                    <p className="text-sm text-white font-['Futura'] md:text-lg">Technology has always interested
                         and excited me both as a source of leisure and as an invaluable tool. I truly believe it has
                         the potential to change the world for the better. My recent dive into the world of coding
                         has really cemented these ideals for me. Problem-solving and creating bring me such joy and
                         I'm excited to bring my creativity and logic into this challenging new career path.</p>
                 </div>
-                <div className="relative flex-wrap-col m-auto justify-center space-x-3 z-10 w-fit md:w-2/3 top-60 md:top-52 p-6">
+                <div className="collapse md:visible md:relative md:flex md:m-auto md:justify-center md:space-x-5 md:z-10 md:w-fit md:left-[5%] md:w-3/4 md:top-60 md:top-52 md:p-6">
+                    <FontAwesomeIcon icon="fa-brands fa-php" className="collapse md:visible text-5xl text-white"/>
+                    <FontAwesomeIcon icon="fa-brands fa-js" className="collapse md:visible text-5xl text-white"/>
+                    <FontAwesomeIcon icon="fa-brands fa-html5" className="collapse md:visible text-5xl text-white"/>
+                    <FontAwesomeIcon icon="fa-brands fa-css3-alt" className="collapse md:visible text-5xl text-white"/>
+                    <FontAwesomeIcon icon="fa-brands fa-react" className="collapse md:visible text-5xl text-white"/>
+                    <FontAwesomeIcon icon="fa-brands fa-bootstrap" className="collapse md:visible text-5xl text-white"/>
+                    <FontAwesomeIcon icon="fa-brands fa-node" className="collapse md:visible text-5xl text-white"/>
+                    <FontAwesomeIcon icon="fa-brands fa-sass" className="collapse md:visible text-5xl text-white mx-3"/>
+                    <FontAwesomeIcon icon="fa-brands fa-git-alt" className="collapse md:visible text-5xl text-white mr-3"/>
+                    <div className="flex">
+                        <img src={postman} alt="postman" className="collapse md:visible w-11 mr-2"/>
+                        <img src={mongodb} alt="mongodb" className="collapse md:visible w-11 mr-2"/>
+                        <img src={mysql} alt="mysql" className="collapse md:visible w-11"/>
+                    </div>
+                </div>
+                <div className="md:hidden relative flex-wrap-col m-auto justify-center space-x-3 z-10 w-fit md:w-2/3 top-24 md:top-52 p-6">
                     <FontAwesomeIcon icon="fa-brands fa-php" className="text-4xl text-white"/>
                     <FontAwesomeIcon icon="fa-brands fa-js" className="text-4xl text-white"/>
                     <FontAwesomeIcon icon="fa-brands fa-html5" className="text-4xl text-white"/>
@@ -39,7 +55,7 @@ export default function About() {
                     <FontAwesomeIcon icon="fa-brands fa-react" className="text-4xl text-white"/>
                     <FontAwesomeIcon icon="fa-brands fa-bootstrap" className="text-4xl text-white"/>
                 </div>
-                <div className="relative flex m-auto justify-center w-fit top-60 p-0">
+                <div className="md:hidden relative flex m-auto justify-center w-fit top-24 p-0">
                     <FontAwesomeIcon icon="fa-brands fa-node" className="text-4xl text-white"/>
                     <FontAwesomeIcon icon="fa-brands fa-sass" className="text-4xl text-white mx-3"/>
                     <FontAwesomeIcon icon="fa-brands fa-git-alt" className="text-4xl text-white mr-3"/>
