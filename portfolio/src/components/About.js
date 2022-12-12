3fb2d11fd865b6ddf929d24e85420354e1f7b824
Georgeoak03 <101403429+Georgeoak03@users.noreply.github.com>
2022-12-12 14:36:33 +0000

    Splash/About tidied

--- portfolio/src/components/About.js
+++ portfolio/src/components/About.js
@@ -26,26 +26,26 @@ export default function About() {
     }, [control, inView]);
 
     return (
-        <section id="about-me" className="bg-zinc-900">
+        <section id="about-me">
             <m.div ref={ref}
                  variants={boxVariant}
                  initial="hidden"
                  animate={control}
-                className="relative">
-                <div className="invisible absolute z-0 bg-[#222A23] w-[70%] h-[475px] top-44 left-[15%] lg:visible lg:top-32 lg:h-[700px] lg:left-[20%]"></div>
-                <div className="absolute z-0 bg-[#263d28] w-[175px] lg:w-[300px] h-6 top-28 left-[25%] lg:h-10 lg:left-[16%] lg:top-44 lg:w-[450px]"></div>
-                <div className="absolute w-[85px] h-6 z-0 bg-[#263d28] top-44 left-[25%] lg:hidden"></div>
-                <div className="w-36 flex absolute z-0 left-[55%] top-40 lg:w-72 lg:left-[60%] lg:top-72">
+                className="relative min-h-screen">
+                <div className="invisible lg:visible absolute z-0 bg-[#222A23] w-[70%] top-28  h-[700px] left-[20%]"></div>
+                <div className="absolute z-0 bg-[#263d28] w-[175px] lg:w-[300px] h-6 top-28 left-[25%] lg:h-8 lg:left-[16%] lg:top-40"></div>
+                <div className="absolute lg:hidden w-[85px] z-0 bg-[#263d28] h-6 top-44 left-[25%]"></div>
+                <div className="flex absolute z-0 left-[55%] w-36  top-40 lg:w-72 lg:left-[60%] lg:top-72">
                     <img src={photoOfMe} alt="George" className="grayscale opacity-25 lg:opacity-75"/>
                 </div>
                 <div className="relative z-10 w-4/5 top-20 left-[20%] lg:top-32 lg:left-[12%]">
-                    <h1 className="text-6xl w-1/5 lg:w-full lg:text-12xl lg:text-8xl text-white font-greatSailor">
+                    <h1 className="text-white font-greatSailor text-6xl w-1/5 lg:w-full lg:text-12xl">
                         About me.
                     </h1>
                 </div>
-                <div className="relative flex-wrap-col left-[10%] top-40 z-10 w-4/5 md:w-[400px] md:left-[15%] md:top-72 lg:w-1/3 lg:top-40 lg:left-[22%]">
-                    <p className="text-sm text-white font-['Futura'] lg:text-lg lg:text-lg">I am a motivated and passionate
-                        Full Stack Developer. Outside of catchin' 'mons, chuckin' blue spikey shells and levatating
+                <div className="relative z-10 flex-wrap-col left-[10%] top-40 w-4/5 md:left-[25%] md:w-1/3 lg:left-[25%]">
+                    <p className="text-sm text-white font-['Futura'] lg:text-lg">I am a motivated and passionate
+                        Full Stack Developer. Outside of catchin' 'mons, chuckin' blue spikey shells and levitating
                         spaceships out of swamps, I have a love for the outdoors and getting to see the world.</p>
                     <br />
                     <p className="text-sm text-white font-['Futura'] lg:text-lg">Technology has always interested
@@ -54,7 +54,7 @@ export default function About() {
                         has really cemented these ideals for me. Problem-solving and creating bring me such joy and
                         I'm excited to bring my creativity and logic into this challenging new career path.</p>
                 </div>
-                <div className="collapse lg:visible lg:relative lg:flex lg:m-auto lg:justify-center lg:space-x-5 lg:z-10 lg:w-fit lg:left-[5%] lg:w-3/4 md:top-80 lg:top-44 lg:p-6 xl:top-64">
+                <div className="collapse lg:visible relative flex m-auto justify-center space-x-5 z-10 w-fit left-[5%] w-3/4 top-44 p-6 xl:top-72">
                     <FontAwesomeIcon icon="fa-brands fa-php" className="collapse lg:visible lg:text-4xl xl:text-5xl text-white"/>
                     <FontAwesomeIcon icon="fa-brands fa-js" className="collapse lg:visible lg:text-4xl xl:text-5xl text-white"/>
                     <FontAwesomeIcon icon="fa-brands fa-html5" className="collapse lg:visible lg:text-4xl xl:text-5xl text-white"/>
@@ -64,30 +64,30 @@ export default function About() {
                     <FontAwesomeIcon icon="fa-brands fa-node" className="collapse lg:visible lg:text-4xl xl:text-5xl text-white"/>
                     <FontAwesomeIcon icon="fa-brands fa-sass" className="collapse lg:visible lg:text-4xl xl:text-5xl text-white mx-3"/>
                     <FontAwesomeIcon icon="fa-brands fa-git-alt" className="collapse lg:visible lg:text-4xl xl:text-5xl text-white mr-3"/>
-                    <div className="flex">
-                        <img src={postman} alt="postman" className="collapse lg:visible w-11 mr-2"/>
-                        <img src={mongodb} alt="mongodb" className="collapse lg:visible w-11 mr-2"/>
-                        <img src={mysql} alt="mysql" className="collapse lg:visible w-11"/>
+                    <div className="flex collapse lg:visible">
+                        <img src={postman} alt="postman" className="w-11 mr-2"/>
+                        <img src={mongodb} alt="mongodb" className="w-11 mr-2"/>
+                        <img src={mysql} alt="mysql" className="w-11"/>
                     </div>
                 </div>
-                <div className="lg:hidden relative flex-wrap-col m-auto justify-center space-x-3 z-10 w-fit lg:w-2/3 top-24 md:top-64 lg:top-52 p-6">
-                    <FontAwesomeIcon icon="fa-brands fa-php" className="text-4xl text-white"/>
-                    <FontAwesomeIcon icon="fa-brands fa-js" className="text-4xl text-white"/>
-                    <FontAwesomeIcon icon="fa-brands fa-html5" className="text-4xl text-white"/>
-                    <FontAwesomeIcon icon="fa-brands fa-css3-alt" className="text-4xl text-white"/>
-                    <FontAwesomeIcon icon="fa-brands fa-react" className="text-4xl text-white"/>
-                    <FontAwesomeIcon icon="fa-brands fa-bootstrap" className="text-4xl text-white"/>
+                <div className="lg:hidden relative flex-wrap-col m-auto justify-center space-x-3 w-fit top-24 lg:w-2/3 lg:top-52 p-6 text-4xl text-white">
+                    <FontAwesomeIcon icon="fa-brands fa-php"/>
+                    <FontAwesomeIcon icon="fa-brands fa-js"/>
+                    <FontAwesomeIcon icon="fa-brands fa-html5"/>
+                    <FontAwesomeIcon icon="fa-brands fa-css3-alt"/>
+                    <FontAwesomeIcon icon="fa-brands fa-react"/>
+                    <FontAwesomeIcon icon="fa-brands fa-bootstrap"/>
                 </div>
-                <div className="lg:hidden relative flex m-auto justify-center w-fit top-24 p-0 md:top-64">
-                    <FontAwesomeIcon icon="fa-brands fa-node" className="text-4xl text-white"/>
-                    <FontAwesomeIcon icon="fa-brands fa-sass" className="text-4xl text-white mx-3"/>
-                    <FontAwesomeIcon icon="fa-brands fa-git-alt" className="text-4xl text-white mr-3"/>
+                <div className="lg:hidden relative flex m-auto justify-center w-fit top-24 p-0 lg:top-64 text-4xl text-white">
+                    <FontAwesomeIcon icon="fa-brands fa-node"/>
+                    <FontAwesomeIcon icon="fa-brands fa-sass" className="mx-3"/>
+                    <FontAwesomeIcon icon="fa-brands fa-git-alt" className="mr-3"/>
                     <img src={postman} alt="postman" className="w-9 mr-1"/>
                     <img src={mongodb} alt="mongodb" className="w-9 mr-1"/>
                     <img src={mysql} alt="mysql" className="w-9"/>
                 </div>
             </m.div>
-            <div className="relative top-36 w-2/5 h-56 lg:h-96 left-[10%] md:top-32 md:left-40"></div>
+            <div className="relative h-72 flex"></div>
         </section>
     )
 }
--- portfolio/src/components/Splash.js
+++ portfolio/src/components/Splash.js
@@ -12,12 +12,12 @@ export default function Splash() {
         }
     }
 
-    const item = {
-        visible: { opacity: 1},
-        hidden: { opacity: 0,
-            transition: { duration: 1 }
-        }
-    }
+    // const item = {
+    //     visible: { opacity: 1},
+    //     hidden: { opacity: 0,
+    //         transition: { duration: 1 }
+    //     }
+    // }
 
     const control = useAnimation()
     const [ref, inView] = useInView()
@@ -30,7 +30,7 @@ export default function Splash() {
         }}, [control, inView]);
 
     return (
-        <section id="splash" className="bg-zinc-900 min-h-screen z-999">
+        <section id="splash" className="min-h-screen">
             <m.div ref={ref}
                    variants={boxVariant}
                    initial="hidden"
@@ -38,35 +38,39 @@ export default function Splash() {
                    className="h-auto">
                 <div className="relative">
                     <div className="relative h-fit left-[10%] md:left-[25%] lg:left-[35%] w-screen">
-                        <div className="absolute top-36 lg:top-64 left-1 lg:left-4 w-52 h-52 lg:w-72 lg:h-72 bg-[#3b403b] rounded-full filter blur-xl lg:blur-2xl animate-blob overflow-hidden"></div>
-                        <div className="absolute top-44 lg:top-72 left-10 lg:left-36 w-52 h-52 lg:w-72 lg:h-72 bg-[#30332f] rounded-full filter blur-xl lg:blur-2xl opacity-90 animate-blob animation-delay-2000 overflow-hidden"></div>
-                        <div className="absolute top-72 md:top-96 left-10 lg:left-20 w-52 h-52 lg:w-72 lg:h-72 bg-[#3b403b] rounded-full filter blur-xl lg:blur-3xl animate-blob animation-delay-4000 overflow-hidden"></div>
-                        <div className="invisible lg:visible absolute top-48 -left-52 w-72 h-72 bg-[#3b403b] rounded-full filter blur-2xl animate-blob animation-delay-6000"></div>
-                        <div className="invisible md:visible absolute top-56 -left-40 w-72 h-72 bg-[#3b403b] rounded-full filter blur-2xl animate-blob animation-delay-1000"></div>
-                        <div className="collapse md:visible absolute top-[475px] md:left-24 lg:left-64 w-72 h-72 bg-[#3b403b] rounded-full filter blur-3xl  animate-blob animation-delay-6000"></div>
+                        <div className="absolute bg-[#3b403b] top-36 left-1 w-52 h-52 lg:w-72 lg:h-72 lg:top-64 lg:left-4 rounded-full filter blur-xl lg:blur-2xl animate-blob overflow-hidden"></div>
+                        <div className="absolute bg-[#30332f] top-44 left-10 w-52 h-52 lg:left-36 lg:top-72 lg:w-72 lg:h-72 rounded-full filter blur-xl lg:blur-2xl opacity-90 animate-blob animation-delay-2000 overflow-hidden"></div>
+                        <div className="absolute bg-[#3b403b] top-72 left-10 w-52 h-52 md:top-96 lg:left-20 lg:w-72 lg:h-72 rounded-full filter blur-xl lg:blur-3xl animate-blob animation-delay-4000 overflow-hidden"></div>
+                        <div className="absolute bg-[#3b403b] invisible lg:visible top-48 -left-52 w-72 h-72 rounded-full filter blur-2xl animate-blob animation-delay-6000"></div>
+                        <div className="absolute bg-[#3b403b] invisible md:visible top-56 -left-40 w-72 h-72 rounded-full filter blur-2xl animate-blob animation-delay-1000"></div>
+                        <div className="absolute bg-[#3b403b] invisible md:visible top-[475px] w-72 h-72  md:left-24 lg:left-64 rounded-full filter blur-3xl animate-blob animation-delay-6000"></div>
                     </div>
-                    <div className="relative max-w-xs z-10 top-28 lg:top-36 left-[10%] md:left-[15%] lg:left-[25%]">
-                        <h1 className="lg:text-[175px] md:text-[150px] text-8xl text-white font-greatSailor">
+                    <div className="relative z-10 max-w-xs top-28 left-[10%] md:left-[15%] lg:left-[25%] lg:top-36">
+                        <h1 className="text-8xl md:text-[150px] lg:text-[175px] text-white font-greatSailor">
                             HELLO THERE!
                         </h1>
                     </div>
-                    <div className="relative z-10 w-52 lg:w-auto inline-block left-[20%] mr-[20%] mt-28 md:left-[32%] lg:mt-36 lg:left-[45%] text-left">
-                        <p className="text-3xl text-zinc-300 font-greatSailor md:text-5xl italic w-auto inline-block">I'm
-                        <p className="text-[#85B97C] text-5xl not-italic">
-                            <span className="h-auto text-6xl md:text-7xl">G</span>eorge
-                            <span className="text-6xl md:text-7xl"> O</span>akley,
+                    <div className="relative z-10 w-52 left-[20%] mr-[20%] mt-28 md:left-[32%] lg:mt-36 lg:left-[45%] lg:w-auto text-left">
+                        <p className="text-3xl text-zinc-300 font-greatSailor italic inline-block w-auto md:text-5xl">I'm
+                            <p className="text-[#85B97C] text-5xl not-italic">
+                                <span className="text-6xl md:text-7xl">G</span>eorge
+                                <span className="text-6xl md:text-7xl"> O</span>akley,
+                            </p>
                         </p>
+                        <p className="text-3xl md:text-5xl text-zinc-300 font-greatSailor italic">
+                            <span className="whitespace-nowrap w-auto inline-block">a FULL</span>-STACK
                         </p>
-                        <p className="text-3xl md:text-5xl text-zinc-300 font-greatSailor italic"><span className="whitespace-nowrap w-auto inline-block">a FULL</span>-STACK</p> <p className="text-3xl md:text-5xl text-zinc-300 font-greatSailor italic w-auto inline-block">DEVELOPER.</p>
+                        <p className="text-3xl md:text-5xl text-zinc-300 font-greatSailor italic w-auto inline-block">DEVELOPER.
+                        </p>
                     </div>
                 </div>
-                <div className="absolute pin-b bottom-10 lg:bottom-20 z-10 left-1/2 w-fit m-auto flex justify-center hover:animate-bounce">
+                <div className="absolute pin-b bottom-10 lg:bottom-5 z-10 left-1/2 w-fit m-auto flex justify-center">
                     <a href="#about-me">
-                        <FontAwesomeIcon icon="fa-solid fa-arrow-down" className="text-gray-700 hover:text-white text-4xl transition ease-in-out duration-1000 transform"/>
+                        <FontAwesomeIcon icon="fa-solid fa-arrow-down" className="text-gray-700 hover:text-white text-4xl hover:animate-bounce transition ease-in-out duration-1000 transform"/>
                     </a>
                 </div>
             </m.div>
-            <div className="relative top-36 w-2/5 h-32 left-[10%] md:top-32 md:left-40"></div>
+            <div className="relative h-72"></div>
         </section>
     )
 }
